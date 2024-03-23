e49b230a77e5274563e98b57d66aa207dd6499ee
Mazed1413 <mamazed1314@gmail.com>
2024-03-23 20:36:47 +0600

    add chart component

--- src/component/Chart/Chart.jsx
+++ src/component/Chart/Chart.jsx
@@ -1,8 +1,22 @@
+import { LineChart, Line } from "recharts";
+
 const Chart = () => {
+  const data = [
+    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page B", uv: 300, pv: 1398, amt: 2210 },
+    { name: "Page C", uv: 200, pv: 9800, amt: 2290 },
+    { name: "Page D", uv: 278, pv: 3908, amt: 2000 },
+    { name: "Page E", uv: 189, pv: 4800, amt: 2181 },
+  ];
   return (
-    <div>
-      <h2 className="bg-amber-100 p-4">This is from chart</h2>
-    </div>
+    <>
+      <h2 className="text-center font-bold text-2xl p-3">Line Chart</h2>
+      <div className="px-10">
+        <LineChart width={400} height={400} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        </LineChart>
+      </div>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -24,7 +23,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
 );
